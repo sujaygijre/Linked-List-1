4172c6395a68a1031ba4582e928a0fc9dca7618a
SUJAY GIJRE <sujaygijre@gmail.com>
2026-01-18 20:58:54 -0500

    Create LLCycle.cpp

---
+++ LLCycle.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *slow=head, *fast=head;
+
+        while (fast!=nullptr && fast->next!=nullptr) {
+            fast = fast->next->next;
+            slow=slow->next;
+            if (slow == fast) {
+                break;
+            }
+        }
+
+        if (fast == nullptr || fast->next == nullptr)
+            return nullptr;
+
+        fast=head;
+
+        while (fast!=slow) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        return slow;
+    }
+};
